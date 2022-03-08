dcd0bdd2e81ef5cbd0d9804f8449e12204bebb34
esubalew.seife@gmail.com <esubalew.seife@gmail.com>
2022-03-08 08:59:25 -0800

    added redux and implemented user state and user action

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import "./header.style.scss";
 import { auth } from "../../firebase/firebase.utils";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { connect } from "react-redux";
 const Header = ({ currentUser }) => {
   return (
     <div className="header">
@@ -31,4 +32,7 @@ const Header = ({ currentUser }) => {
   );
 };
 
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+export default connect(mapStateToProps)(Header);
