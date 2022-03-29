cd5ceb4a7b00206e6d8f82d1f622e7a1129cfd7d
eeyfesubalew <esubalew.seife@gmail.com>
2022-03-28 20:16:34 -0700

    connecting redux state to cart item quantiy

--- src/components/card-item/card-item.component.jsx
+++ src/components/card-item/card-item.component.jsx
@@ -3,14 +3,19 @@ import { ReactComponent as ShopingIcon } from "../../assets/shopping-bag.svg";
 import "./card-item.styles.scss";
 import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.action";
-const CardIcon = ({ toggleCartHidden }) => (
+const CardIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShopingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
-
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+    0
+  ),
+});
 const mapDipatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
-export default connect(null, mapDipatchToProps)(CardIcon);
+export default connect(mapStateToProps, mapDipatchToProps)(CardIcon);
